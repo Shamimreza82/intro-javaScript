680cb8ff5f9d12a62aa726001a8d399f93b5e56c
Shamimreza82 <shamimrezaone@gmail.com>
2023-07-29 12:08:28 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,90 @@
+// var harrMonGiveHim = 1500;
+// var apple1kg = 400 ;
+// var orange1kg = 300 ;
+
+// var totalCost = (apple1kg + orange1kg)
+// var shopkkeperRetun = (harrMonGiveHim - totalCost)     
+
+// console.log (shopkkeperRetun)
+
+// -----------------------------------------------------------------------------
+
+
+// var bangla  = 75.25;
+// var English = 65;
+// var math    = 80;
+// var science = 35.45;
+// var history = 99.50; 
+
+// var totalMarks = (bangla + English + math + science + history);
+// var obtainMarks = totalMarks/5;
+// // console.log (obtainMarks)
+// var final = obtainMarks.toFixed(2);
+// console.log (final)
+// final = parseInt(final)
+// // console.log (final)
+
+
+
+
+
+// var totalMarks =  totalMarks;
+// var
+// console.log ("Obtain Marks"+ " " + total + "  " +  "Marks with divition 5")
+// console.log (totalMarks)
+
+
+
+// -----------------------------------------------------------------------------
+
+
+
+
+// var name1 = "I am going to be"
+// var name2 = "an awesome web devoloper"
+
+// var num3 = name1 + name2 
+// console.log (num3)
+
+
+
+
+
+
+//--------------------------------------------------------------
+
+
+// var give = 119;
+// var divided = 23; 
+
+// var divided = (give % divided)
+// console.log (divided) 
+
+
+
+var bangla  = 34;
+    bangla +=7 
+    console.log(bangla)
+
+
+
+// var bangla  = 34;
+// var english = 35;
+// var math    = 25.56;
+// var history = 45.6;
+// var islam   = 56.46;
+// var science = 55.78; 
+// var total = (bangla + english + math + history + islam + science)
+// var totalMarks = total/6 
+
+// var nameSubject = ("bangla  = 34")
+// console.log(nameSubject)
+
+
+// // console.log (totalMarks)
+
+// var finalMarks = (totalMarks.toFixed(2))
+// console.log (bangla, english, math, history )
+// console.log ( "this is your final marks" + " " + finalMarks)
+
+ 
