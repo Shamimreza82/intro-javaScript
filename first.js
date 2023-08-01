86563f9a43d8e8e934f9ee4e6c2bb680673d88b9
Shamimreza82 <shamimrezaone@gmail.com>
2023-08-01 22:56:48 +0600

    added functions

--- first.js
+++ first.js
@@ -1,168 +1,81 @@
-// var harrMonGiveHim = 1500;
-// var apple1kg = 400 ;
-// var orange1kg = 300 ;
+ /*
+ 1. What is javaScript 
 
-// var totalCost = (apple1kg + orange1kg)
-// var shopkkeperRetun = (harrMonGiveHim - totalCost)     
+        JavaScript is the Programming Language for the Web.
+        JavaScript can update and change both HTML and CSS.
+        JavaScript can calculate, manipulate and validate data. 
 
-// console.log (shopkkeperRetun)
+ 2. How dose js works? (Solved)
 
-// -----------------------------------------------------------------------------
+ 3. What is variable? 
 
 
-// var bangla  = 75.25;
-// var English = 65;
-// var math    = 80;
-// var science = 35.45;
-// var history = 99.50; 
+4. Declare a variable? 
 
-// var totalMarks = (bangla + English + math + science + history);
-// var obtainMarks = totalMarks/5;
-// // console.log (obtainMarks)
-// var final = obtainMarks.toFixed(2);
-// console.log (final)
-// final = parseFloat(final)
-// console.log (final)
+        var name1 = 2;     
+        var name2 = "Reza"  
+        var name3 = true 
 
 
+5. Type of variable
+        var name1 = 2;     
+        var name2 = "Reza"  
+        var name3 = true    
 
+        console.log (typeof(true))
 
 
-// var totalMarks =  totalMarks;
-// var
-// console.log ("Obtain Marks"+ " " + total + "  " +  "Marks with divition 5")
-// console.log (totalMarks)
+6. Primitive data types Non-primitive data types 
 
+        Primitive data types - includes byte, short, int, long, float, double, boolean and char
+        Non-primitive data types - such as String, Arrays and Classes 
 
 
-// -----------------------------------------------------------------------------
+7. avaScript naming conventions
 
+        Names are case-sensitive, lowercase and uppercase are different.
+        Start variable names with a letter, use camelCase for names.
+        Variable names should be self-descriptive, describing the stored value.
+        Boolean variables are usually prefixed with is or has .
 
 
+ 8. Math Operation +, -, *, /, % 
 
-// var name1 = "I am going to be"
-// var name2 = "an awesome web devoloper"
+         Arthmetic operators in JavaScript include:
 
-// var num3 = name1 + name2 
-// console.log (num3)
+        Addition (+)
+        Subtraction (-)
+        Multiplication (*)
+        Exponentiation (**)
+        Modulus (%)
+        Decrement (--)
+        Increment (++)
+        Division (/)
 
+9. Short hand: +=, -=, *=, /= 
 
+        Operator	    Example	          Same As
+            +=	        x += y	        x = x + y
+            -=	        x -= y	        x = x - y
+            *=	        x *= y	        x = x * y
+            /=	        x /= y	        x = x / y
 
+ 10. parseInt, ParseFloat
 
+        parseFloat()	
 
+        considers the decimal point and the digits coming after it	
+        Example - parseFloat() of "3.99" would be 3.99
 
-//--------------------------------------------------------------
+        parseInt()
 
+        3. parseInt() ignores the decimal point and even the digits coming after it
+         example - parseInt of "3.99" would be 3.
 
-// var give = 119;
-// var divided = 23; 
 
-// var divided = (give % divided)
-// console.log (divided) 
+11. toFixed
 
 
 
-// var bangla  = 34;
-//     bangla +=7 
-//     console.log(bangla)
 
 
-
-// var bangla  = 34;
-// var english = 35;
-// var math    = 25.56;
-// var history = 45.6;
-// var islam   = 56.46;
-// var science = 55.78; 
-// var total = (bangla + english + math + history + islam + science)
-// var totalMarks = total/6 
-
-// console.log(nameSubject)
-
-
-// // console.log (totalMarks)
-
-// var finalMarks = (totalMarks.toFixed(2))
-// console.log (bangla, english, math, history )
-// console.log ( "this is your final marks" + " " + finalMarks)
-
- 
-
-
-// var bangla = "15.17"
-
-// bangla = parseFloat(bangla)
-// console.log(bangla)
-
-
-
-
-
-
-// var bangla  = 34;
-// var English = 45.56;
-// var Math    = 34;
-// var Science = 34.67;
-// var Biology = 34;
-// var Islam   = 34.96;
-// var Economic = 34;
-// var history = 34.84;
-
-// var total = (bangla + English + Math + Science + Biology + Islam + Economic + history)
-//  var totalMarks = total/8;
-
-//     totalMarks = totalMarks.toFixed(2)
-//     console.log (totalMarks)
-//     totalMarks = parseFloat(totalMarks)
-//     console.log (totalMarks)
-
-
-
-// var arr = [2, 3 ,4, 5 ,6];
-// console.log(arr[2])
-
-
-// let myArray = [1, 2, 3, 4, 5];
-// console.log(myArray[2]);
-
-// let x = 5;
-// let y = 10;
-// if (x < y) {
-//  console.log("x is less than y");
-// } else {
-//  console.log("x is greater than or equal to y");
-// }
-
-
-
-// var fruits = ['apple', 'banana', 'Orange']
-// console.log(fruits.indexOf("banana"))
-//     console.log(fruits)
-//     fruits.pop()
-//     fruits.push("watermallon")
-//     console.log(fruits)
-
-
-var num1 = 13; 
-var num2 = 79;
-var num3 = 45; 
-var num4 = 9; 
-var num5 = 8; 
-var num6 = 9; 
-
-// if (num3 > num1) {
-//     console.log ( "79")
-// }
-
-// else {
-//     console.log ( "79" )
-// }
-
-
-if (num1 > num3 ) {
-    console.log ("its a tringal")
-}
-
-else {
-    console.log ( "its not a tringel")
-}
---
+++ fs.js
@@ -0,0 +1,180 @@
+// var name = 34; 
+// var pepole = 245 
+// var  bus = pepole % 50; 
+// console.log (bus)
+
+// var car = bus % 11; 
+// console.log (car)
+
+
+// var number = "45.5667778"
+// console.log(typeof number);
+// var number1 = parseFloat(number)
+// console.log (number1)
+// var inte = number1.toFixed(2)
+// console.log (inte)
+// inte = parseFloat(inte)
+// console.log (inte)
+
+
+
+// // console.log(5)
+
+// var x = 5; 
+// console.log(++x)
+
+
+// var name1 = [23, 34, 45, 67, 67, 56];
+// console.log (name1.length)
+// var arrName = name1
+// console.log(arrName)
+
+// name1.push(67)
+// name1.push(67)
+// name1.push(67)
+// name1.push(67)
+// name1.push(67)
+// name1.push(67)
+// name1.push(67)
+// name1.push(67)
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// console.log(name1)
+
+
+
+// var age = 25; 
+
+// if (age >= 18) {
+//     console.log("you are adult")
+// }
+
+// else{
+//     console.log("you are not adult")
+// }
+  
+
+
+
+// if ( mark >= 80){
+//     console.log ("A+")
+//     if (mark)
+// }
+// else if ( mark >= 60){
+//     console.log ("A")
+// }
+// else if ( mark >= 50){
+//     console.log ("B")
+// }
+// else if ( mark >= 33){
+//     console.log ("c")
+// }
+
+// else (
+//     console.log ( "fail"
+//     )
+// )
+
+
+// var bangla = 50; 
+// var english = 69; 
+// var math = 34; 
+// var islam = 45;  
+
+// var total = bangla + english + math + islam
+
+// if ( bangla >= 80) {
+//     console.log ("A")
+// }
+// else if ( bangla >=60) {
+//     console.log ("pass")
+// }
+// else if ( bangla < 60) {
+//     console.log ("fail")
+// }
+// -----------------------------------------------------function------------------------------------------
+
+// function myname() {
+//     console.log ("stand up")
+//     console.log ("stand up")
+//     console.log ("stand up")
+//     console.log ("stand up") 
+// }
+
+
+// myname(); 
+// console.log ("myname us  is shamim Reza ")
+// myname(); 
+// myname(); 
+// console.log ("myname us  is shamim Reza ")
+// myname(); 
+// myname(); 
+// console.log ("myname us  is shamim Reza ")
+// myname(); 
+
+
+ // -----------------------------------------------------paramitter --------------------------------------------
+
+// function add ( number1, number2) {
+//     console.log (number1, number1)
+//     var name1 = number1 + number2
+//     return name1
+// }
+
+
+// var total = add(50, 49)
+// console.log( total)
+
+
+
+// function add (money) {
+//     var pant = 40; 
+//     var total = money / 40; 
+//     return total; 
+// }
+
+// var myGive = 400
+// var totalAmmount = add (myGive);
+// console.log ( "total pant:", totalAmmount)
+
+
+
+// function mangoBuy (myMony) {
+//     var mangoPrise = 20; 
+//     var totalAmmount = myMony / 20; 
+//     return totalAmmount; 
+// }
+
+
+// var myTaka = 400; 
+
+// var total = mangoBuy (myTaka); 
+// console.log (total)
+
+
+
+
+// ---------------------------------------------object-----------------------------
+
+
+var  myCompuer = {
+    brand: "benque",
+    color: "black",
+    price: 40000,  
+}
+
+
+console.log (myCompuer.color)
+
+myCompuer.color = 'red'
+
+console.log (myCompuer)
