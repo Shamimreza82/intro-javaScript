c638f7a2a714737091578d0961ce00d848f96893
Shamimreza82 <shamimrezaone@gmail.com>
2023-08-03 12:01:07 +0600

    update code

--- first.js
+++ first.js
@@ -1,218 +1,649 @@
- /*
- 1. What is javaScript 
 
-        JavaScript is the Programming Language for the Web.
-        JavaScript can update and change both HTML and CSS.
-        JavaScript can calculate, manipulate and validate data. 
+//  1. What is javaScript 
 
- 2. How dose js works? (Solved)
+//         JavaScript is the Programming Language for the Web.
+//         JavaScript can update and change both HTML and CSS.
+//         JavaScript can calculate, manipulate and validate data. 
 
- 3. What is variable? 
+//  2. How dose js works? (Solved)
 
-
-4. Declare a variable? 
-
-        var name1 = 2;     
-        var name2 = "Reza"  
-        var name3 = true 
+//  3. What is variable? 
 
 
-5. Type of variable
-        var name1 = 2;     
-        var name2 = "Reza"  
-        var name3 = true    
+// 4. Declare a variable? 
 
-        console.log (typeof(true))
-
-
-6. Primitive data types Non-primitive data types 
-
-        Primitive data types - includes byte, short, int, long, float, double, boolean and char
-        Non-primitive data types - such as String, Arrays and Classes 
+//         var name1 = 2;     
+//         var name2 = "Reza"  
+//         var name3 = true 
 
 
-7. avaScript naming conventions
+// 5. Type of variable
+//         var name1 = 2;     
+//         var name2 = "Reza"  
+//         var name3 = true    
 
-        Names are case-sensitive, lowercase and uppercase are different.
-        Start variable names with a letter, use camelCase for names.
-        Variable names should be self-descriptive, describing the stored value.
-        Boolean variables are usually prefixed with is or has .
-
-
- 8. Math Operation +, -, *, /, % 
-
-         Arthmetic operators in JavaScript include:
-
-        Addition (+)
-        Subtraction (-)
-        Multiplication (*)
-        Exponentiation (**)
-        Modulus (%)
-        Decrement (--)
-        Increment (++)
-        Division (/)
-
-9. Short hand: +=, -=, *=, /= 
-
-        Operator	    Example	          Same As
-            +=	        x += y	        x = x + y
-            -=	        x -= y	        x = x - y
-            *=	        x *= y	        x = x * y
-            /=	        x /= y	        x = x / y
-
- 10. parseInt, ParseFloat
-
-        parseFloat()	
-
-        considers the decimal point and the digits coming after it	
-        Example - parseFloat() of "3.99" would be 3.99
-
-        parseInt()
-
-        3. parseInt() ignores the decimal point and even the digits coming after it
-         example - parseInt of "3.99" would be 3.
+//         console.log (typeof(true))
 
 
-11. toFixed
+// 6. Primitive data types Non-primitive data types 
+
+//         Primitive data types - includes byte, short, int, long, float, double, boolean and char
+//         Non-primitive data types - such as String, Arrays and Classes 
 
 
+// 7. avaScript naming conventions
+
+//         Names are case-sensitive, lowercase and uppercase are different.
+//         Start variable names with a letter, use camelCase for names.
+//         Variable names should be self-descriptive, describing the stored value.
+//         Boolean variables are usually prefixed with is or has .
+
+
+//  8. Math Operation +, -, *, /, % 
+
+//          Arthmetic operators in JavaScript include:
+
+//         Addition (+)
+//         Subtraction (-)
+//         Multiplication (*)
+//         Exponentiation (**)
+//         Modulus (%)
+//         Decrement (--)
+//         Increment (++)
+//         Division (/)
+
+// 9. Short hand: +=, -=, *=, /= 
+
+//         Operator	    Example	          Same As
+//             +=	        x += y	        x = x + y
+//             -=	        x -= y	        x = x - y
+//             *=	        x *= y	        x = x * y
+//             /=	        x /= y	        x = x / y
+
+//  10. parseInt, ParseFloat
+
+//         parseFloat()	
+
+//         considers the decimal point and the digits coming after it	
+//         Example - parseFloat() of "3.99" would be 3.99
+
+//         parseInt()
+
+//         3. parseInt() ignores the decimal point and even the digits coming after it
+//          example - parseInt of "3.99" would be 3.
+
+
+// 11. toFixed
 
 
 
-<<<<<<< HEAD
+// // var bangla  = 34;
+// // var english = 35;
+// // var math    = 25.56;
+// // var history = 45.6;
+// // var islam   = 56.46;
+// // var science = 55.78; 
+// // var total = (bangla + english + math + history + islam + science)
+// // var totalMarks = total/6 
 
-// var bangla  = 34;
-// var english = 35;
-// var math    = 25.56;
-// var history = 45.6;
-// var islam   = 56.46;
-// var science = 55.78; 
-// var total = (bangla + english + math + history + islam + science)
-// var totalMarks = total/6 
-
-// console.log(nameSubject)
+// // console.log(nameSubject)
 
 
-// // console.log (totalMarks)
+// // // console.log (totalMarks)
 
-// var finalMarks = (totalMarks.toFixed(2))
-// console.log (bangla, english, math, history )
-// console.log ( "this is your final marks" + " " + finalMarks)
+// // var finalMarks = (totalMarks.toFixed(2))
+// // console.log (bangla, english, math, history )
+// // console.log ( "this is your final marks" + " " + finalMarks)
 
  
 
 
-// var bangla = "15.17"
+// // var bangla = "15.17"
 
-// bangla = parseFloat(bangla)
-// console.log(bangla)
-
-
+// // bangla = parseFloat(bangla)
+// // console.log(bangla)
 
 
 
 
-// var bangla  = 34;
-// var English = 45.56;
-// var Math    = 34;
-// var Science = 34.67;
-// var Biology = 34;
-// var Islam   = 34.96;
-// var Economic = 34;
-// var history = 34.84;
-
-// var total = (bangla + English + Math + Science + Biology + Islam + Economic + history)
-//  var totalMarks = total/8;
-
-//     totalMarks = totalMarks.toFixed(2)
-//     console.log (totalMarks)
-//     totalMarks = parseFloat(totalMarks)
-//     console.log (totalMarks)
 
 
+// // var bangla  = 34;
+// // var English = 45.56;
+// // var Math    = 34;
+// // var Science = 34.67;
+// // var Biology = 34;
+// // var Islam   = 34.96;
+// // var Economic = 34;
+// // var history = 34.84;
 
-// var arr = [2, 3 ,4, 5 ,6];
-// console.log(arr[2])
+// // var total = (bangla + English + Math + Science + Biology + Islam + Economic + history)
+// //  var totalMarks = total/8;
 
-
-// let myArray = [1, 2, 3, 4, 5];
-// console.log(myArray[2]);
-
-// let x = 5;
-// let y = 10;
-// if (x < y) {
-//  console.log("x is less than y");
-// } else {
-//  console.log("x is greater than or equal to y");
-// }
+// //     totalMarks = totalMarks.toFixed(2)
+// //     console.log (totalMarks)
+// //     totalMarks = parseFloat(totalMarks)
+// //     console.log (totalMarks)
 
 
 
-// var fruits = ['apple', 'banana', 'Orange']
-// console.log(fruits.indexOf("banana"))
-//     console.log(fruits)
-//     fruits.pop()
-//     fruits.push("watermallon")
-//     console.log(fruits)
+// // var arr = [2, 3 ,4, 5 ,6];
+// // console.log(arr[2])
+
+
+// // let myArray = [1, 2, 3, 4, 5];
+// // console.log(myArray[2]);
+
+// // let x = 5;
+// // let y = 10;
+// // if (x < y) {
+// //  console.log("x is less than y");
+// // } else {
+// //  console.log("x is greater than or equal to y");
+// // }
+
+
+
+// // var fruits = ['apple', 'banana', 'Orange']
+// // console.log(fruits.indexOf("banana"))
+// //     console.log(fruits)
+// //     fruits.pop()
+// //     fruits.push("watermallon")
+// //     console.log(fruits)
 
 
 
 
-// var num1 = 13; 
-// var num2 = 79;
-// var num3 = 45; 
-// var num4 = 9; 
-// var num5 = 8; 
-// var num6 = 9; 
+// // var num1 = 13; 
+// // var num2 = 79;
+// // var num3 = 45; 
+// // var num4 = 9; 
+// // var num5 = 8; 
+// // var num6 = 9; 
 
-// // if (num3 > num1) {
-// //     console.log ( "79")
+// // // if (num3 > num1) {
+// // //     console.log ( "79")
+// // // }
+
+// // // else {
+// // //     console.log ( "79" )
+// // // }
+
+
+// // if (num1 > num3 ) {
+// //     console.log ("its a tringal")
 // // }
 
 // // else {
-// //     console.log ( "79" )
+// //     console.log ( "its not a tringel")
 // // }
 
 
-// if (num1 > num3 ) {
-//     console.log ("its a tringal")
+// // var totalMarks = ["85", "66", "95", "56", "40"]
+
+
+// // var gread1 = ["80", "60", "50", "40", "39"]
+
+// // var me = 85; 
+// // var tom = 66; 
+// // var jane = 95; 
+// // var peter = 56; 
+// // var jon = 40; 
+
+
+
+
+
+// let marks = 65;
+
+// switch (marks) {
+//     case 80 <= 100:
+//         console.log(" Wow nice mark")
+//         break;
+//     case 60:
+//         console.log(" You should stop")
+//         break;
+//     case 40:
+//         console.log(" You Should cross the road")
+//         break;
+
+//     default:
+//         console.log("not your gread")
+// }
+// =======
+
+
+
+
+// --------------------------------------------------------------------------------------------------\
+
+
+
+
+
+// var name = 34; 
+// var pepole = 245 
+// var  bus = pepole % 50; 
+// console.log (bus)
+
+// var car = bus % 11; 
+// console.log (car)
+
+
+// var number = "45.5667778"
+// console.log(typeof number);
+// var number1 = parseFloat(number)
+// console.log (number1)
+// var inte = number1.toFixed(2)
+// console.log (inte)
+// inte = parseFloat(inte)
+// console.log (inte)
+
+
+
+// // console.log(5)
+
+// var x = 5; 
+// console.log(++x)
+
+
+// var name1 = [23, 34, 45, 67, 67, 56];
+// console.log (name1.length)
+// var arrName = name1
+// console.log(arrName)
+
+// name1.push(67)
+// name1.push(67)
+// name1.push(67)
+// name1.push(67)
+// name1.push(67)
+// name1.push(67)
+// name1.push(67)
+// name1.push(67)
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// console.log(name1)
+
+
+
+// var age = 25; 
+
+// if (age >= 18) {
+//     console.log("you are adult")
 // }
 
-// else {
-//     console.log ( "its not a tringel")
+// else{
+//     console.log("you are not adult")
 // }
 
 
-// var totalMarks = ["85", "66", "95", "56", "40"]
 
 
-// var gread1 = ["80", "60", "50", "40", "39"]
+// if ( mark >= 80){
+//     console.log ("A+")
+//     if (mark)
+// }
+// else if ( mark >= 60){
+//     console.log ("A")
+// }
+// else if ( mark >= 50){
+//     console.log ("B")
+// }
+// else if ( mark >= 33){
+//     console.log ("c")
+// }
 
-// var me = 85; 
-// var tom = 66; 
-// var jane = 95; 
-// var peter = 56; 
-// var jon = 40; 
+// else (
+//     console.log ( "fail"
+//     )
+// )
+
+
+// var bangla = 50; 
+// var english = 69; 
+// var math = 34; 
+// var islam = 45;  
+
+// var total = bangla + english + math + islam
+
+// if ( bangla >= 80) {
+//     console.log ("A")
+// }
+// else if ( bangla >=60) {
+//     console.log ("pass")
+// }
+// else if ( bangla < 60) {
+//     console.log ("fail")
+// }
+// -----------------------------------------------------function------------------------------------------
+
+// function myname() {
+//     console.log ("stand up")
+//     console.log ("stand up")
+//     console.log ("stand up")
+//     console.log ("stand up") 
+// }
+
+
+// myname(); 
+// console.log ("myname us  is shamim Reza ")
+// myname(); 
+// myname(); 
+// console.log ("myname us  is shamim Reza ")
+// myname(); 
+// myname(); 
+// console.log ("myname us  is shamim Reza ")
+// myname(); 
+
+
+// -----------------------------------------------------paramitter --------------------------------------------
+
+// function add ( number1, number2) {
+//     console.log (number1, number1)
+//     var name1 = number1 + number2
+//     return name1
+// }
+
+
+// var total = add(50, 49)
+// console.log( total)
+
+
+
+// function add (money) {
+//     var pant = 40; 
+//     var total = money / 40; 
+//     return total; 
+// }
+
+// var myGive = 400
+// var totalAmmount = add (myGive);
+// console.log ( "total pant:", totalAmmount)
+
+
+
+// function mangoBuy (myMony) {
+//     var mangoPrise = 20; 
+//     var totalAmmount = myMony / 20; 
+//     return totalAmmount; 
+// }
+
+
+// var myTaka = 400; 
+
+// var total = mangoBuy (myTaka); 
+// console.log (total)
+
+
+
+
+// ---------------------------------------------object-----------------------------
+
+
+// var  myCompuer = {
+//     brand: "benque",
+//     color: "black",
+//     price: 40000,  
+// }
+
+
+// console.log (myCompuer.color)
+// myCompuer.price = 5000;
+// // console.log (myCompuer)
+
+// myCompuer ["color"] = "white"
+// console.log (myCompuer)
+
+
+// --------------------------------------------problem 2------------------------------
+
+// function make_ave(assinment1, assinment2, assinment3) {
+//         var total = assinment1 + assinment2 + assinment3 
+//         var totalavarag = total / 3; 
+//         return totalavarag; 
+// }
+// var assinmentMarks1 = 60; 
+// var assinmentMarks2 = 56; 
+// var assinmentmarks3 = 53; 
+// var myMarks = make_ave(assinmentMarks1, assinmentMarks2, assinmentmarks3)
+// console.log (myMarks)
+
+// var fixed = myMarks.toFixed(2); 
+// console.log (fixed)
+
+// var flote = parseFloat(fixed)
+
+// console.log (flote)
+
+
+
+// --------------------------------------------problem 1------------------------------
+
+
+// var make_avg = [60, 56, 53]; 
+
+// console.log (make_avg)
 
 
 
 
 
-let marks = 65;
+//  var arry = [60, 56, 55];
 
-switch (marks) {
-    case 80 <= 100:
-        console.log(" Wow nice mark")
-        break;
-    case 60:
-        console.log(" You should stop")
-        break;
-    case 40:
-        console.log(" You Should cross the road")
-        break;
+// function calculateAverage(array) {
+//     var total = 0;
+//     var count = 0;
+//     array.forEach(function(item, index) {
+//         total += item;
+//         count++;
+//     });
+//     return total / count;
+// }
 
-    default:
-        console.log("not your gread")
-}
-=======
->>>>>>> origin/main
+// console.log(calculateAverage(arry));
+
+
+
+
+
+
+
+
+// const arr = [60, 56, 55];
+// const average = arr.reduce((a, b) => a + b, 0) / arr.length;
+
+// console.log(average);
+
+
+
+
+// const arr = [60, 56, 55];
+// var sum = 0;
+
+// for (var number of arr) {
+//     sum += number;
+// }
+
+// average = sum / arr.length;
+
+// console.log(average);
+
+
+// --------------------------------------------problem 4------------------------------
+
+
+
+// var signal = "green" ; 
+
+// if ( signal == "red") {
+//     console.log (" You may be in danger")
+// } 
+// else if ( signal == "yellow"){
+//     console.log (" You should stop")
+// }
+// else if ( signal == "green"){
+//     console.log (" You Should cross the road")
+// }
+
+
+
+
+// let signal = "  " ;
+
+// switch (signal) {
+//   case "red":
+//     console.log (" You may be in danger")
+//     break;
+//   case "green":
+//         console.log (" You should stop")
+//     break;
+//   case "yellow":
+//     console.log (" You Should cross the road")
+//     break;
+
+//   default:
+//     signal = "Unknown";
+// }
+
+
+// --------------------------------------------problem 5------------------------------
+
+
+// let marks = ttt;
+
+// switch (marks) {
+//     case 80 <= 100:
+//         console.log(" Wow nice mark")
+//         break;
+//     case 60:
+//         console.log(" You should stop")
+//         break;
+//     case 40:
+//         console.log(" You Should cross the road")
+//         break;
+
+//     default:
+//         console.log("not your gread")
+// }
+
+
+
+// var myObject = {
+//     color: "Black",
+//     price:  450, 
+//     hight: 4.6, 
+
+// }
+
+
+// var
+
+// var penCount = myObject.price;
+// console.log(myObject)
+
+
+// function inchToFeet (inch) {
+//     const convirt = inch / 12; 
+//     return convirt; 
+// }
+
+// console.log(inchToFeet (345))
+
+
+
+// function GetArrayNumbers(numbers) {
+//     for (let i = 0; i < numbers.le; i++) {
+//         const index = i;
+//         const element = numbers[index]
+//         console.log(index, element)
+//     }
+// }
+
+
+// GetArrayNumbers()
+
+
+
+
+
+// const myNumbers= [ 23, 45, 34, 45, 45, 56, 45, 34]
+// console.log(myNumbers); 
+
+
+
+
+// function inchTofeet (inche) {
+//     let feet = inche / 12; 
+//     return feet; 
+// }
+
+// const resultInch = 84; 
+// const incheFinal = inchTofeet(resultInch); 
+// console.log (incheFinal)
+
+
+
+
+
+
+// function kmToMiles (km) {
+//     let miles = km * 1.60934; 
+//     return miles; 
+// }
+
+// const KmInput = 2; 
+// const totalMinles = kmToMiles(KmInput)
+// console.log (totalMinles)
+
+
+
+// function evenNumber ( number) {
+//     if (number % 2) {
+//        console.log ("this is even Number")
+//     }
+
+//     else {
+//         console.log ("this is odd Number")
+//     }
+
+// }
+
+
+// console.log (evenNumber(4))
+
+
+// --------------------------leap Year-----------------------------
+
+function checkLeapYear(year) {
+    
+        if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+            console.log(year + ' is a leap year');
+        } else {
+            console.log(year + ' is not a leap year');
+        }
+    }
+    
+    console.log (checkLeapYear(1500))
+    
+    // --------------------------The array in odd an even numbers-----------------------------
+    
+    function getSum (num1) {
+        for (let i = 0; i < num1.length; i++) {
+            const index = i; 
+            console.log (index);
+        }
+    }
+    
+    
+    const myNumbers = [23, 34, 56, 45, 34, 45, 23, 56, 34, 77, ]
+    getSum(myNumbers);
+    
